ddd4cd49c90c75606e664c8c7d1a61c197e15db0
Bryan Tiete Tanoue <bryan_tanoue@hotmail.com>
2021-01-26 09:03:55 -0300

    Arrays e dicionários em js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 
 
+var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+
+
+/* 
+var fruta = {nome:"maça", cor: "vermelha"};
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+/*
+var lista = ["maça", "Pêra", "Laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+/*
 var nome = "Bryan";
 var idade = 20;
 var idade2 = 10; 
@@ -9,3 +29,4 @@ var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(idade + idade2);
 console.log(frase.replace("Japão", "Brasil"));
+*/
