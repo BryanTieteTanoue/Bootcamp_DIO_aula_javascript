8474defbb1d35c33e45bfdd8bddbcde672fc166e
Bryan Tiete Tanoue <bryan_tanoue@hotmail.com>
2021-01-26 09:17:24 -0300

    Consicionais, laços e Date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 
 
+var d = new Date();
+alert(d.getMonth() + 1); //retorna o mês menos 1, assim tem que somar +1 para vir o correto
+
+/*
+var cont;
+for (cont = 0; cont <= 5; cont++ ){
+    alert(cont);
+}*/
+
+/*
+var cont = 0;
+while(cont < 5) {
+    console.log(cont);
+    cont++;
+}*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade")
+}*/
+
+/*
 var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}];
 console.log(frutas);
 console.log(frutas[1].nome);
-
+*/
 
 /* 
 var fruta = {nome:"maça", cor: "vermelha"};
